1ac13f5743eed14dcd4da8d3881adaaf47eeed97
RakeshkumarThera <rakeshkumarthera@gmail.com>
2024-08-30 15:09:52 -0400

    Dependency Injection code only

---
+++ src/main/java/com/rakesh/springcoredemo/Coach.java
@@ -0,0 +1,5 @@
+package com.rakesh.springcoredemo;
+
+public interface Coach {
+    public String getDailyWorkout();
+}
---
+++ src/main/java/com/rakesh/springcoredemo/CricketCoach.java
@@ -0,0 +1,11 @@
+package com.rakesh.springcoredemo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class CricketCoach implements Coach{
+    @Override
+    public String getDailyWorkout() {
+        return "Practice Bowling for 15 minutes daily...";
+    }
+}
