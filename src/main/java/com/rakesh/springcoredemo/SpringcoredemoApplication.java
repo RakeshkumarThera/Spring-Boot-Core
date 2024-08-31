56f030c706054c892acf8e9aa410a2358acbdb16
RakeshkumarThera <rakeshkumarthera@gmail.com>
2024-08-30 23:53:51 -0400

    reset to constructor injection

--- src/main/java/com/rakesh/springcoredemo/SpringcoredemoApplication.java
+++ src/main/java/com/rakesh/springcoredemo/SpringcoredemoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 //@SpringBootApplication(scanBasePackages = {"com.rakesh.springcoredemo",   //this for package scanning
 //		"com.rakesh.util"
 //})
+
 public class SpringcoredemoApplication {
 
 	public static void main(String[] args) {
