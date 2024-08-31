f540596a6981bb3a44c2226b8d9e319bb98648c2
RakeshkumarThera <rakeshkumarthera@gmail.com>
2024-08-31 00:10:32 -0400

    using Primary

--- src/main/java/com/rakesh/springcoredemo/common/TennisCoach.java
+++ src/main/java/com/rakesh/springcoredemo/common/TennisCoach.java
@@ -1,8 +1,10 @@
 package com.rakesh.springcoredemo.common;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+@Primary
 public class TennisCoach implements Coach{
     @Override
     public String getDailyWorkout() {
